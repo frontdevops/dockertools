ccf9845d39029292af48e59190defef0b60e29fe
Alexander Majorov <alexander.majorov@gmail.com>
2016-01-03 17:35:56 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,21 @@
+# add this file in your .bashrc file:
+# source docker.sh
+
+dockerip() {
+    sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
+}
+
+dockerips() {
+	sudo docker ps
+	echo -e "\nIP for runed containers:"
+    for dock in $(sudo docker ps | tail -n +2 | cut -d" " -f1)
+    do
+        local dock_ip=$(dockerip $dock)
+        if [[ -n "$dock_ip" ]]
+        then
+            echo $dock = $dock_ip
+        fi
+    done
+}
+
+#EOF#
