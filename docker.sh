590bbf4a69ea479fa340ac5bc7ebd2209bc7b6f4
Alexander Majorov <alexander.majorov@gmail.com>
2016-01-03 17:50:45 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ dockerip() {
 
 dockerips() {
     sudo docker ps
-    echo -e "\nIP for runed containers:"
+    echo -e "\nIP for runned containers:"
     for dock in $(sudo docker ps | tail -n +2 | cut -d" " -f1)
     do
         local dock_ip=$(dockerip $dock)
