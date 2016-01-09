0468037aa3e63ebb38b1567f5af4e026b2a09dd2
Alexander Majorov <alexander.majorov@gmail.com>
2016-01-09 23:17:41 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,17 +2,17 @@
 # source docker.sh
 
 dockerip() {
-	docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
+    docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
 }
 
 dockerips() {
-	for dock in $(docker ps | tail -n +2 | cut -d" " -f1)
-	do
-		local dock_ip=$(dockerip $dock)
-		[[ -n "$dock_ip" ]] && regex="s/$dock\s\{3\}/${dock:0:3}  $dock_ip/g;$regex"
-	done
-
-	docker ps -a | sed -e "$regex"
+    for dock in $(docker ps | tail -n +2 | cut -d" " -f1)
+    do
+        local dock_ip=$(dockerip $dock)
+        [[ -n "$dock_ip" ]] && regex="s/$dock\s\{3\}/${dock:0:3}  $dock_ip/g;$regex"
+    done
+    
+    docker ps -a | sed -e "$regex"
 }
 
 #EOF#
